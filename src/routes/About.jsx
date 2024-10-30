47b141cbd0203ff3902a988007993b5680fa0365
Jonathan Zacarias <Jonathanazacarias@gmail.com>
2024-10-30 11:17:11 -0700

    Switch Nav, content, and footer placement.

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,11 +1,14 @@
 import Footer from "../components/footer/Footer";
 import NavBar from "../components/navBar/NavBar";
+import { Container } from "react-bootstrap";
 
 function About() {
   return (
     <div>
       <NavBar />
-      <h1>About Page</h1>
+      <Container>
+        <h1>About Me</h1>
+      </Container>
       <Footer />
     </div>
   );
