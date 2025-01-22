6244d3e75f9f04eb3af5415e0bdf91f00a8cea3a
jonathanazacarias <jonathanazacarias@gmail.com>
2025-01-21 18:17:02 -0800

    add to resume page.

--- src/routes/Resume.jsx
+++ src/routes/Resume.jsx
@@ -1,13 +1,17 @@
-import NavBar from "../components/navBar/NavBar"
+import Footer from "../components/footer/Footer";
+import NavBar from "../components/navBar/NavBar";
+import { Container } from "react-bootstrap";
 
 function Resume() {
-
-    return (
-        <div>
-            <NavBar/>
-            <h1>Resume</h1>
-        </div>
-    )
+  return (
+    <div>
+      <NavBar />
+      <Container>
+        <h1>Resume</h1>
+      </Container>
+      <Footer />
+    </div>
+  );
 }
 
-export default Resume
+export default Resume;
