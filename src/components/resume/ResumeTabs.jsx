2fd0360064c3cb74fca8abf9f9d3fa26fa8fa8ff
jonathanazacarias <jonathanazacarias@gmail.com>
2025-01-26 17:16:41 -0800

    Complete "skills", "honors" and "other" tab content.

--- src/components/resume/ResumeTabs.jsx
+++ src/components/resume/ResumeTabs.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import ResumeExperience from "./ResumeExperience";
 import { v4 as uuidv4 } from "uuid";
 import Education from "./Education";
+import Skill from "./Skill";
+import Other from "./Other";
 
 function ResumeTabs(props) {
   const { experienceList, educationList, skillsList, honorsList, otherList } =
@@ -19,26 +21,49 @@ function ResumeTabs(props) {
               date={experience.date}
               roleList={experience.roleList}
             />
-            <hr/>
+            <hr />
           </div>
         ))}
       </Tab>
       <Tab eventKey="education" title="Education & Training">
-        {
-          educationList.map((edu) => (
-            <div key={uuidv4()}>
-              <Education institution={edu.institution} location={edu.location} date={edu.date} summary={edu.summary}/>
-            </div>
-          ))}
+        {educationList?.map((edu) => (
+          <div key={uuidv4()}>
+            <Education
+              institution={edu.institution}
+              location={edu.location}
+              date={edu.date}
+              summary={edu.summary}
+            />
+            <hr />
+          </div>
+        ))}
       </Tab>
       <Tab eventKey="skills" title="Skills">
-        Tab content for Contact
+        {skillsList?.map((skill) => (
+          <div key={uuidv4()}>
+            <Skill
+              skill={skill.skill}
+              skillExampleList={skill.skillExampleList}
+            />
+            <hr />
+          </div>
+        ))}
       </Tab>
       <Tab eventKey="honors" title="Honors">
-        Tab content for Contact
+        {honorsList?.map((honor) => (
+          <div key={uuidv4()}>
+            <p>{honor}</p>
+            <hr />
+          </div>
+        ))}
       </Tab>
       <Tab eventKey="other" title="Other">
-        Tab content for Contact
+        {
+          otherList?.map((other) => (
+            <div key={uuidv4()}>
+              <Other title={other.title} list={other.list}/>
+            </div>
+          ))}
       </Tab>
     </Tabs>
   );
