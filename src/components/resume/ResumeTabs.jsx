6f61a652d55ebb82e04e1844647fd64a41cd3b01
jonathanazacarias <jonathanazacarias@gmail.com>
2025-01-26 16:29:48 -0800

    Add all content for Education and Training tab.

--- src/components/resume/ResumeTabs.jsx
+++ src/components/resume/ResumeTabs.jsx
@@ -3,6 +3,7 @@ import Tabs from "react-bootstrap/Tabs";
 import { Link } from "react-router-dom";
 import ResumeExperience from "./ResumeExperience";
 import { v4 as uuidv4 } from "uuid";
+import Education from "./Education";
 
 function ResumeTabs(props) {
   const { experienceList, educationList, skillsList, honorsList, otherList } =
@@ -22,7 +23,14 @@ function ResumeTabs(props) {
           </div>
         ))}
       </Tab>
-      <Tab eventKey="education" title="Education & Training"></Tab>
+      <Tab eventKey="education" title="Education & Training">
+        {
+          educationList.map((edu) => (
+            <div key={uuidv4()}>
+              <Education institution={edu.institution} location={edu.location} date={edu.date} summary={edu.summary}/>
+            </div>
+          ))}
+      </Tab>
       <Tab eventKey="skills" title="Skills">
         Tab content for Contact
       </Tab>
