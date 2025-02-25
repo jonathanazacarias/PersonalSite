6cdd787a6bdef1b2a97c52e7600f497155adfaf9
jonathanazacarias <40703079+jonathanazacarias@users.noreply.github.com>
2025-02-24 18:34:28 -0800

    Create the door button group feature for the home page.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,15 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import DoorButton from "./doorButton/DoorButton";
+import DoorGroup from "./doorGroup/DoorGroup";
 
 export default function Hero() {
   return (
     <Container>
       <div className="text-center">
         <p className="fs-1">HELLO</p>
-        <p>I'M A</p>
+        <p >I'M A</p>
       </div>
-      <div className="text-center">
-        <DoorButton
-          link="/About"
-          closedDoorSrc="src/components/doorButton/doorImages/door.svg"
-          openDoorSrc="src/components/doorButton/doorImages/pulled-door.svg"
-        ></DoorButton>
-        <DoorButton
-          link="/projects/project/2"
-          closedDoorSrc="src/components/doorButton/doorImages/software-engineer-door.svg"
-          openDoorSrc="src/components/doorButton/doorImages/software-engineer-door-pushed.svg"
-        ></DoorButton>
-        <DoorButton
-          link=""
-          closedDoorSrc="src/components/doorButton/doorImages/door.svg"
-          openDoorSrc="src/components/doorButton/doorImages/pulled-door.svg"
-        ></DoorButton>
-      </div>
+      <DoorGroup></DoorGroup>
     </Container>
   );
 }
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -4,6 +4,9 @@ import Image from 'react-bootstrap/Image';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+import githubsvg from "./navImages/github.svg";
+import linkedinsvg from "./navImages/linkedin.svg";
+
 export default function NavBar() {
 
     return (
@@ -15,8 +18,8 @@ export default function NavBar() {
                     <Nav.Link as={Link} to='../about'>About</Nav.Link>
                     <Nav.Link as={Link} to='../projects'>Projects</Nav.Link>
                     <Nav.Link as={Link} to='../resume'>Resume</Nav.Link>
-                    <Nav.Link as={Link} to='https://github.com/jonathanazacarias' target="_blank"><Image src='src\components\navBar\navImages\github.svg'/></Nav.Link>
-                    <Nav.Link as={Link} to='https://www.linkedin.com/in/jonathan-a-zacarias/' target="_blank"><Image src='src\components\navBar\navImages\linkedin.svg'/></Nav.Link>
+                    <Nav.Link as={Link} to='https://github.com/jonathanazacarias' target="_blank"><Image src={githubsvg}/></Nav.Link>
+                    <Nav.Link as={Link} to='https://www.linkedin.com/in/jonathan-a-zacarias/' target="_blank"><Image src={linkedinsvg}/></Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
